3d50d817ff065289140caa257ccb9b33fdb55b02
xumengyang <xumengyang@yit.com>
2024-04-19 14:16:47 +0800

    feat: UIView add init backgroundColor

--- Source/UIView+Extension.swift
+++ Source/UIView+Extension.swift
@@ -24,4 +24,10 @@ public extension UIView {
             return 0
         }
     }
+    
+    convenience init(backgroundColor: UIColor?) {
+        self.init()
+        self.backgroundColor = backgroundColor
+    }
+    
 }
